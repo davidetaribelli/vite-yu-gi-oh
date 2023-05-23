7e9cd343285160f842428ac2360dbf2c54ff71af
davidetaribelli <davide.taribelli@gmail.com>
2023-05-23 15:16:35 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<template>
+    <header>
+        <div id="boxHeader">
+            <img src="../../public/Yugioh_anime_logo.webp" alt="logo">
+            <h1>Yu-Gi-Oh Api</h1>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use '../style.scss' as *;
+
+#boxHeader {
+    @include flex(row, start, center);
+    height: 4rem;
+    padding: 1em;
+
+    img {
+        height: 90%;
+    }
+
+    h1 {
+        font-weight: 100;
+    }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    name: "AppMain"
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <div class="mainHead">
+                <h4>Found 39 cards</h4>
+            </div>
+            <div class="boxMain">
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped lang="scss">
+@use '../style.scss' as *;
+
+main {
+    background-color: #d48f38;
+    padding: 3.5rem;
+
+    .container {
+        width: 1080px;
+        margin: 0 auto;
+        padding: 3rem;
+        background-color: white;
+
+        h4 {
+            background-color: #212529;
+            color: white;
+            padding: 1rem;
+        }
+
+        .boxMain {
+            @include flex(row, space-between, center, wrap);
+            gap: 1em;
+
+            .card {
+                width: calc(100% / 5 - 1em);
+                height: 388px;
+                background-color: red;
+            }
+        }
+    }
+}
+</style>

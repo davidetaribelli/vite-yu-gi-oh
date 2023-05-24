5cb96cca8e6bf310a0ad7b8cedeed74de7927e79
davidetaribelli <davide.taribelli@gmail.com>
2023-05-24 15:32:40 +0200

    add select

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,20 +1,29 @@
 <script>
+import axios from 'axios';
 import { store } from '../data/store'
 export default {
     name: "AppMain",
     data() {
         return {
-            store
+            store,
+            arrayArchetype: [],
+            currentArchetype: null
         }
-    }
+    },
+
 }
 </script>
 
 <template>
     <main>
+        <select @change="searhType" v-model="currentArchetype">
+            <option v-for="cardType in store.cards" :value="cardType.archetype">
+                {{ cardType.archetype }}
+            </option>
+        </select>
         <div class="container">
             <div class="mainHead">
-                <h4>Found 39 cards</h4>
+                <h4>Found 40 cards</h4>
             </div>
             <div class="boxMain">
                 <div v-for="element, i in store.cards" class="card">
@@ -25,7 +34,7 @@ export default {
                         <h5>{{ element.name }}</h5>
                     </div>
                     <div class="type">
-                        <small>{{ element.type }}</small>
+                        <small>{{ element.archetype }}</small>
                     </div>
                 </div>
             </div>
@@ -39,6 +48,13 @@ export default {
 main {
     background-color: #d48f38;
     padding: 3.5rem;
+
+    select {
+        margin-bottom: 2rem;
+        padding: 1em;
+        border: 0;
+        border-radius: 1em;
+    }
 
     .container {
         width: 1080px;
